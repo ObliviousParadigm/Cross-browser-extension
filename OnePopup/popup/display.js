37ec6a9cf550e8e92d1731532a8f35896a11baa7
ObliviousParadigm <adarshsdda@gmail.com>
2020-02-15 18:24:08 +0530

    Made a very basic version of the final extension

--- OnePopup/popup/display.js
+++ OnePopup/popup/display.js
@@ -1,3 +1,4 @@
+// FIRST TRY
 // function dispTabs(tabs) {
 //     var addTabs = document.querySelector('#tabList');
 //     for (let tab in tabs) {
@@ -10,25 +11,73 @@
 // // var addTabs = document.querySelector('#tabList');
 // var querying = browser.tabs.query({ currentWindow: true });
 // dispTabs(querying);
+// --------------------------------------------------------------
 
 
+// SECOND TRY
+// function getTabs() {
+//     return browser.tabs.query({ currentWindow: true });
+// }
 
+// function dispTabs() {
+//     var tabList = getTabs();
+//     var addToList = document.querySelector("#tabList");
+//     var tab;
+//     for (tab in tabList) {
+//         var newLink = document.createElement("a");
+//         newLink.setAttribute("href", tab.url);
+//         newLink.innerHTML = tab.title
+//         addToList.appendChild(newLink);
+//         // console.log(tab.url);
+//     }
+// }
 
-function getTabs() {
-    return browser.tabs.query({ currentWindow: true });
+// dispTabs()
+// --------------------------------------------------------------
+
+// THIRD TRY
+
+// Execute listTabs when display.html is loaded
+document.addEventListener("DOMContentLoaded", listTabs);
+
+function getCurrentWindowTabs() {
+	// Return tabs.Tab object for tabs in the current window
+
+	// ONLY ACTIVE WINDOW
+	// return browser.tabs.query({ currentWindow: true });
+	
+	// EVERY WINDOW BUT THE ACTIVE WINDOW
+	// return browser.tabs.query({ currentWindow: false });
+
+	// ALL WINDOWS
+	return browser.tabs.query({});
 }
 
-function dispTabs() {
-    var tabList = getTabs();
-    var addToList = document.querySelector("#tabList");
-    var tab;
-    for (tab in tabList) {
-        var newLink = document.createElement("a");
-        newLink.setAttribute("href", tab.url);
-        newLink.innerHTML = tab.title
-        addToList.appendChild(newLink);
-        // console.log(tab.url);
-    }
-}
+function listTabs() {
+	getCurrentWindowTabs().then(function (tabs) {
+		let tabsList = document.getElementById('tabList');
+		let currentTabs = document.createDocumentFragment();
+		// let limit = 5;
+		// let counter = 0;
 
-dispTabs()
+		// Clear the content of tabList to keep refreshing the data 
+		// everytime you click on the extension
+		tabsList.textContent = '';
+
+		for (let tab of tabs) {
+			// if (counter <= limit) {
+			let tabLink = document.createElement('a');
+			let br = document.createElement('br');
+
+			tabLink.textContent = tab.title || tab.id;
+
+			tabLink.setAttribute('href', tab.id);
+			tabLink.classList.add('switchTabs');
+			currentTabs.appendChild(tabLink);
+			currentTabs.appendChild(br);
+			// }
+			// counter += 1;
+		}
+		tabsList.appendChild(currentTabs);
+	});
+}
