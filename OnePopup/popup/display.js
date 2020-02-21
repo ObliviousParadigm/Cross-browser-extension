4594c0e123392fc88adcd5b97ab67f82ee099990
ObliviousParadigm <adarshsdda@gmail.com>
2020-02-22 00:25:04 +0530

    Removed function to display QR Code, adjusted size of body, improved the look

--- OnePopup/popup/display.js
+++ OnePopup/popup/display.js
@@ -14,21 +14,6 @@ function getTabs() {
 	return browser.tabs.query({});
 }
 
-// Function for displaying the QR Code 
-function dispQR(tabLink) {
-	// console.log(tabLink);
-	document.body.textContent = '';
-	var code = kjua(
-		{
-			render: 'image',
-			text: tabLink
-		}
-	)
-	code.setAttribute('width: 100%');
-	code.setAttribute('height: 100%');
-	document.querySelector('body').appendChild(code);
-}
-
 function listTabs() {
 	getTabs().then(function (tabs) {
 		let tabsList = document.getElementById('tabList');
@@ -99,7 +84,13 @@ function listTabs() {
 			// btn.onclick = dispQR(tabLink.getAttribute('href'));
 			btn.onclick = function () {
 				// console.log(tabLink);
-				document.body.textContent = '';
+				let body = document.querySelector('body');
+				let deets = document.createElement('p');
+				let back = document.createElement('button');
+
+				body.textContent = '';
+				back.textContent = 'Display all the websites';
+
 				let code = kjua(
 					{
 						render: 'canvas',
@@ -107,14 +98,24 @@ function listTabs() {
 						size: 250,
 					});
 
-				let deets = document.createElement('p');
+				// deets.setAttribute('style', 'white-space: pre;');
 
-				deets.textContent = tabLink.textContent;
-				deets.classList.add('text-center', 'text-muted');
+				// Use textContent instead
+				// ---------------------------------------------------------
+				deets.classList.add('text-center', 'text-muted', 'text-break');
+				back.classList.add('text-center', 'text-wrap', 'alert', 'alert-danger');
+				// back.setAttribute('style', '');
+				body.setAttribute('style', 'height: 400px;');
 
-				document.querySelector('body').appendChild(code);
-				document.querySelector('body').appendChild(br);
-				document.querySelector('body').appendChild(deets);
+				deets.innerHTML = 'Please scan the QR Code with<br>your phone camera or<br>a QR code reader';
+				// ---------------------------------------------------------
+
+				// back.addEventListener("click", func);
+
+				body.appendChild(code);
+				body.appendChild(br);
+				body.appendChild(deets);
+				body.appendChild(back);
 			};
 
 			if (tab.active) {
